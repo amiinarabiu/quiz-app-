8241eeb4fc6a544b577bff5d6252bb7cf8fd8f2d
amiinarabiu <77628533+amiinarabiu@users.noreply.github.com>
2023-01-05 19:26:40 +0100

    Add files via upload

---
+++ QuestionAnswer.java
@@ -0,0 +1,43 @@
+package com.example.myapplicationzzz;
+
+public class QuestionAnswer {
+
+    public static String question[] = {
+            "Which team won the 2022 FIFA World Cup?",
+            "Who is the CEO of the company Twitter?",
+            "What year did basketball legend Kobe Bryant die?",
+            "Which one of these is not a programming language?",
+            "How many elements are in the periodic table of elements?",
+            "What artist has the most streams on Spotify in 2022?",
+            "How many dots appear on a pair of dice?",
+            "Who is the world’s highest-paid athlete in 2021?",
+            "How many stars are on the Chinese flag?",
+            "What Netflix show had the most streaming views in 2021?"
+    };
+
+    public static String choices[] [] = {
+            {"Brazil", "Japan", "Argentina", "Nigeria"},
+            {"Amina Rabiu", "Stevie Wonders", "Micheal Jackson", "Elon Musk"},
+            {"1999", "2003", "2020", "2021"},
+            {"Java", "Javascript", "C++", "HTML"},
+            {"108", "221", "118", "114"},
+            {"Wiz Khalifa", "Drake", "Davido", "CKay"},
+            {"40", "49", "42", "45"},
+            {"Serena Williams", "Amina Rabiu", "Marcus Rashford", "Connor McGregor"},
+            {"3", "2", "1", "4"},
+            {"Peaky Blinders", "Euphoria", "Squid Games", "Game of Thrones"}
+    };
+
+    public static String correctAnswers[] = {
+            "Argentina",
+            "Elon Musk",
+            "2021",
+            "HTML",
+            "118",
+            "Drake",
+            "42",
+            "Connor McGregor",
+            "3",
+            "Squid Games"
+    };
+}
